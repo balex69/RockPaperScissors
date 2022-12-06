81a719bea0860f6174315596d03029b3ce9f8387
balex69 <119514045+balex69@users.noreply.github.com>
2022-12-06 17:21:51 +0100

    Push, import from previous c# project

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //importer depuis c#; à retravailler + classes pour java (+ images/boutons etc)
+        /*Exemple de pierre feuille ciseaux
+Random random = new Random();
+bool play = true;
+String player;
+String computer;
+String response;
+
+while (play)
+{
+    player = "";
+    computer = "";
+    response = "";
+
+    while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+    {
+
+        Console.WriteLine("Enter ROCK, PAPER, or SCISSORS: ");
+        player = Console.ReadLine();
+        player = player.ToUpper();
+    }
+
+    switch (random.Next(1,4))
+    {
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
+
+    Console.WriteLine("Player :"+player);
+    Console.WriteLine("Computer :"+computer);
+
+    switch(player)
+    {
+        case "ROCK":
+            if (computer == "ROCK")
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else if (computer == "PAPER")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("You win!");
+            }
+            break;
+        case "PAPER":
+            if (computer == "ROCK")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (computer == "PAPER")
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            else
+            {
+                Console.WriteLine("You lose!");
+            }
+            break;
+        case "SCISSORS":
+            if (computer == "ROCK")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else if (computer == "PAPER")
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+            }
+            break;
+    }
+    Console.WriteLine("Want to play again? Y/N");
+    response = Console.ReadLine();
+    if (response.ToUpper() == "Y")
+    {
+        play = true;
+    }
+    else
+    {
+        play = false;
+    }
+    Console.WriteLine("Thank you for playing!! :)");
+}*/
+    }
+}
